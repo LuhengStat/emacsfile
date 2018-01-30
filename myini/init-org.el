b5b801d153ef15930115901b3380f3370f546a5a
LuhengStat <30485426+LuhengStat@users.noreply.github.com>
2018-01-31 00:05:33 +0800

    little change: org color order

--- myini/init-org.el
+++ myini/init-org.el
@@ -8,6 +8,29 @@
 	("CANCELED" . (:foreground "blue" :weight bold))))
 
 
+(require 'org-ac)
+(org-ac/config-default)
+
+;; auto indent in the org-mode
+(setq org-startup-indented t)
+
+;; continue with the clock
+(setq org-clock-continuously nil)
+
+(setq org-html-validation-link nil)
+
+
+(eval-after-load 'org
+  (progn
+    (define-key org-mode-map (kbd "S-C-<left>") nil)
+    (define-key org-mode-map (kbd "S-C-<right>") nil)
+    (define-key org-mode-map (kbd "S-C-<down>") nil)
+    (define-key org-mode-map (kbd "S-C-<up>") nil)
+    (define-key org-mode-map (kbd "M-<left>") nil)
+    (define-key org-mode-map (kbd "M-<right>") nil)
+    (define-key org-mode-map (kbd "C-'") nil)))
+
+
 ;; agenda views
 (setq org-agenda-files '("/Users/wlh/Documents/Personal/Org/"))
 (custom-set-faces
@@ -34,30 +57,6 @@
 (global-set-key (kbd "C-c t") 'org-agenda)
 (global-set-key [?\s-t] 'MyDef-org-agenda-view)
 
-
-(require 'org-ac)
-(org-ac/config-default)
-
-
-
-;; auto indent in the org-mode
-(setq org-startup-indented t)
-
-;; continue with the clock
-(setq org-clock-continuously nil)
-
-(setq org-html-validation-link nil)
-
-
-(eval-after-load 'org
-  (progn
-    (define-key org-mode-map (kbd "S-C-<left>") nil)
-    (define-key org-mode-map (kbd "S-C-<right>") nil)
-    (define-key org-mode-map (kbd "S-C-<down>") nil)
-    (define-key org-mode-map (kbd "S-C-<up>") nil)
-    (define-key org-mode-map (kbd "M-<left>") nil)
-    (define-key org-mode-map (kbd "M-<right>") nil)
-    (define-key org-mode-map (kbd "C-'") nil)))
 
 
 (provide 'init-org)
