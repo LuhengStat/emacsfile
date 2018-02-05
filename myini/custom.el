1cbe9e0dfe2e95c9e3e7e4d139328e2b2d12ec1c
LuhengStat <30485426+LuhengStat@users.noreply.github.com>
2018-02-05 10:49:56 +0800

    org clocking with frame titile

--- myini/custom.el
+++ myini/custom.el
@@ -29,6 +29,7 @@
    (quote
     ("~/Documents/Learning/FlashIdeas.org" "~/Documents/Personal/Org/othernotes.org" "/Users/wlh/Documents/Personal/Org/diary.org" "/Users/wlh/Documents/Personal/Org/goodhabit.org" "/Users/wlh/Documents/Personal/Org/workdiary.org")))
  '(org-agenda-restore-windows-after-quit t)
+ '(org-clock-clocked-in-display (quote frame-title))
  '(org-clock-report-include-clocking-task t)
  '(package-selected-packages
    (quote
