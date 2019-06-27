16f01aa4345aa13d80f77eaec351c19301c93e9b
LuhengStat <wlh0426@gmail.com>
2019-06-27 21:53:33 +0800

    using different font

--- myini/init-ui.el
+++ myini/init-ui.el
@@ -167,30 +167,22 @@
 (add-hook 'inferior-ess-mode-hook 'my-buffer-face-mode-smaller)
 
 
-;;;; 设置编辑环境
+;;;; Set env of edit
 (setenv "LC_ALL" "en_US.UTF-8")
-;; 设置emacs 使用 utf-8
 (setq locale-coding-system 'utf-8)
-;; 设置键盘输入时的字符编码
 (set-keyboard-coding-system 'utf-8)
 (set-selection-coding-system 'utf-8)
-;; 文件默认保存为 utf-8
 (set-buffer-file-coding-system 'utf-8)
 (set-default buffer-file-coding-system 'utf-8)
 (set-default-coding-systems 'utf-8)
-;; 解决粘贴中文出现乱码的问题
 (set-clipboard-coding-system 'utf-8)
-;; 终端中文乱码
 (set-terminal-coding-system 'utf-8)
 (modify-coding-system-alist 'process "*" 'utf-8)
 (setq default-process-coding-system '(utf-8 . utf-8))
-;; 解决文件目录的中文名乱码
+;; Path 
 (setq-default pathname-coding-system 'utf-8)
-;;设置文件名编码
+;; file name
 (setq file-name-coding-system 'utf-8)
-;;设置目录路径名编码
-(setq path-name-coding-system 'utf-8)
-
 
 
 ;; always open files in the same window
